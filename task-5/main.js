7394591a89928450d687cf561d35e441093035a3
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-07-01 09:58:49 -0700

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,10 +1,22 @@
-// Use this matrix for solution
-
-let matrix = [
+// let matrix = [
     [134, 475, 30, 424],
     [303, 151, 419, 235],
     [248, 166, 90, 42],
     [318, 237, 184, 36]
 ];
+let left = 0;
+let right = 0;
+for (let i = 0; i <matrix.length; i++) {
+   for (let j = 0; j < matrix[i].length; j++) {
+    if (i == j) {
+        left = left+ matrix[i][j]
+    }
+    else if (j == matrix.length-(i+1)) {
+        right = right + matrix[i][j]
+    }
 
-// Write code here. 
+   }
+    
+}
+console.log(left);
+console.log(right);
