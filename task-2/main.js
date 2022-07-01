74e9342f1de19555273aba065034481bd937b21a
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-07-01 08:23:06 -0700

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,4 +1,13 @@
 // Use this array for solution
 let array = [3, 5, -7, 7, 5, -9, -4]
 
-// Write code here. 
+let arr2 = [];
+count = 0;
+for (let i = 0; i < arr1.length; i++) {
+   if (!arr2.includes(arr1[i])) {
+     arr2.push(arr1[i])
+     count++;
+   }
+    
+}
+console.log(count);
