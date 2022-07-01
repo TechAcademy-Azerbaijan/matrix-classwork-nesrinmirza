38faa76f5a59491767b45f15ebd992ebfb6a4dd7
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-07-01 08:18:40 -0700

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,5 +1,11 @@
 // Use this arrays for solution
 let array_1 = [3, 1, 3, 4, 2, 4, 12]
 let array_2 = [4, 15, 43, 1, 15, 1]
-
-// Write code here. 
+let arr3 =[]
+for (let i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])) {
+        arr3.push(arr1[i])
+    }
+    
+}
+console.log(arr3);
